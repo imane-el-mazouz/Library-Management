cbc0e08b41d50e0856ed18150a00f920b8247557
Imane El Mazouz <elmazouz.imane@gmail.com>
2025-02-23 23:25:02 +0000

    modif de dash de bibliothequere

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import LibrarianDashboard from './pages/LibrarianDashboard';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<LibrarianDashboard />} />
             </Routes>
         </Router>
     );
